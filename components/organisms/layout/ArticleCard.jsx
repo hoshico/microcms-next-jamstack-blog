3facd379c9ae88baab75defbe9fb93376e2a945c
hoshico <melancholinista.yasu@gmail.com>
2022-01-09 20:16:48 +0900

    rm console.log

--- components/organisms/layout/ArticleCard.jsx
+++ components/organisms/layout/ArticleCard.jsx
@@ -1,14 +1,34 @@
 /* eslint-disable */
-import { Box, Stack, Text } from '@chakra-ui/react';
+import { Box, Stack, Text } from '@chakra-ui/react'
+import Link from 'next/link'
 import { memo } from 'react'
 
 export const ArticleCard = memo((props) => {
-  const { blogId, blogTitle } = props;
-  return (  
-    <Box w="260px" h="260px" bg="white" shadow="md" p={8} _hover={{ cursor: "pointer", opacity: 0.8}}>
-      <Stack textAlign="center">
-        <Text fontSize="lg" fontWeight="bold">{blogTitle}</Text>
-      </Stack>
-    </Box>
+  const { blogId, blogTitle, blogCategory } = props
+  return (
+    <Link href={`/blog/${blogId}`}>
+      <Box
+        w="260px"
+        h="260px"
+        bg="white"
+        shadow="md"
+        p={8}
+        mr={5}
+        _hover={{ cursor: 'pointer', opacity: 0.8 }}
+      > 
+        <Stack textAlign="left">
+          {blogCategory && (
+              <Text fontSize="sm" fontWeight="bold">
+                {blogCategory.name}
+              </Text>
+            )}
+        </Stack>
+        <Stack textAlign="center">
+          <Text fontSize="lg" fontWeight="bold">
+            {blogTitle}
+          </Text>
+        </Stack>
+      </Box>
+    </Link>
   )
-});
+})
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,4 @@
 import { Heading, Wrap, WrapItem } from "@chakra-ui/react";
-import Link from "next/link";
 import { client } from "../libs/client";
 import { HeaderLayout } from "../components/templates/HeaderLayout";
 import { ArticleCard } from "../components/organisms/layout/ArticleCard";
@@ -8,17 +7,14 @@ import { ArticleCard } from "../components/organisms/layout/ArticleCard";
 
 export default function Home(props) {
   const { blog } = props;
-  console.log(blog);
+  //console.log(blog);
   return (
     <>
     <HeaderLayout></HeaderLayout>
-    <Wrap p={{ base: 4, md: 10}}>
+    <Wrap p={{ base: 8, md: 10}}>
       {blog.map((blog) => (
         <WrapItem key={blog.id} mx="auto">
-          {/*<Link href={`/blog/${blog.id}`}>
-              <a>{blog.title}</a>
-          </Link>*/}
-          <ArticleCard blogId={blog.id} blogTitle={blog.title}></ArticleCard>
+          <ArticleCard blogCategory={blog.category} blogId={blog.id} blogTitle={blog.title} ></ArticleCard>
         </WrapItem>
       ))}
     </Wrap>
