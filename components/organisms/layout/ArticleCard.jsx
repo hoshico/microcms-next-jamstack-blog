3f876179172cb763144578f1ef43f6dfe3456a0b
hoshico <melancholinista.yasu@gmail.com>
2022-01-31 09:15:57 +0900

    fix articleCard.jsx

--- components/organisms/layout/ArticleCard.jsx
+++ components/organisms/layout/ArticleCard.jsx
@@ -7,9 +7,7 @@ import { useGetCategoryImage } from '../../hooks/useGetCategoryImage'
 
 export const ArticleCard = memo((props) => {
   const { blogId, blogTitle, blogCategory } = props
-  if(blogCategory) {
-    useGetCategoryImage(blogCategory.name);
-  }
+  
   return (
     <Link href={`/blog/${blogId}`}>
       <Box
@@ -24,7 +22,8 @@ export const ArticleCard = memo((props) => {
         <Stack textAlign="center">
           {blogCategory ? (<Image w="300px" src={`/${blogCategory.name}.png`} />) : (<Image w="300px" src="/etc.png" />)}
         </Stack>
-        {/*{blogCategory ? (<Image src={`/${blogCategory.name}.png`} width={300} height={300} />) : (<Image w="300px" src="/etc.png" width={300} height={300}/>)}*/}
+        {/*<useGetCategoryImage category={blogCategory.name} />*/}
+        {/*{blogCategory ? <useGetCategoryImage category={blogCategory.name} /> : (<Image w="300px" src="/etc.png" width={300} height={300}/>)}*/}
         <Stack textAlign="center">
           <Text fontSize="lg" fontWeight="bold" mt={2} bg="gray.300">
             {blogTitle}
